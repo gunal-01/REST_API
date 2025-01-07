440f280b4efffec95cb8c775251ccedf89c57839
gunal-01 <gunalprakash006@gmail.com>
2025-01-07 23:10:21 +0530

    learned about getting and updating data in the psql database.

--- src/student/controller.js
+++ src/student/controller.js
@@ -1,7 +1,25 @@
+const pool = require('../../db');
+
 const getStudents = (req, res) => {
-    console.log('Getting students')
-}
+    pool.query("SELECT * FROM students", (error, results) => {
+        if (error) throw error;
+        res.status(200).json(results.rows);
+    });
+};
+
+const updateStudents = (req, res) => {
+    const { id, email } = req.body;
+    pool.query(
+        "UPDATE students SET email = $1 WHERE id = $2",
+    [email, id],
+    (error, results) => {
+        if(error) throw error;
+        res.status(200).send(`Student modified with ID: ${id}`);
+    }
+    );
+};
 
 module.exports = {
     getStudents,
-}
+    updateStudents,
+};
--- src/student/routes.js
+++ src/student/routes.js
@@ -3,6 +3,6 @@ const controller = require('./controller');
 const router =  Router();
 
 router.get("/", controller.getStudents);
-
+router.post("/", controller.updateStudents);
 
 module.exports = router;
